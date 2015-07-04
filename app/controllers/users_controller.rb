b5bb93ddca2defe087d3b30c80aa4924b96c7ab4
dave-pryce <dp@Davids-MacBook-Pro.local>
2015-07-04 13:25:46 +1000

    updates

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,13 +10,13 @@ class UsersController < ApplicationController
       session[:user_id] = @user.id
       redirect_to '/'
     else
-      redirect_to '/signup'
+      render 'new'
     end
   end
 
   private
   def user_params
-    params.require(:user).permit(:first_name, :last_name, :email, :password)
+    params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation)
   end
 
 end

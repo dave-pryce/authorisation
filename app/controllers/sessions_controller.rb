1206f6b6f73c7b1474ae4f56a19eaea8a132f765
David Pryce <dpryce@seek.com.au>
2015-07-07 08:18:54 +1000

    html fixed - basic sign up and in working.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,7 +7,7 @@ class SessionsController < ApplicationController
   @user = User.find_by_email(params[:session][:email])
   if @user && @user.authenticate(params[:session][:password])
     session[:user_id] = @user.id
-    redirect_to '/'
+    redirect_to @user
   else
     flash[:danger] = 'Invalid email/password combination'
     render 'new'

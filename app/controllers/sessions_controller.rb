bf2f54dd0533257100613d35653110e1410c0da5
dave-pryce <dp@Davids-MacBook-Pro.local>
2015-07-07 20:09:22 +1000

    sign and login working with errors.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,7 +9,7 @@ class SessionsController < ApplicationController
     session[:user_id] = @user.id
     redirect_to @user
   else
-    flash[:danger] = 'Invalid email/password combination'
+    flash.now[:danger] = 'Invalid email/password combination'
     render 'new'
   end
 end

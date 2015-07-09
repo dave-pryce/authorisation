e57461f9fb90e32a6aa9aea4d71a2bd223f54e09
David Pryce <dpryce@seek.com.au>
2015-07-10 09:01:25 +1000

    restricted edit to correct user only

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   before_action :require_user, only: [:show]
-  before_action :correct_user, only: [:show]
+  before_action :correct_user, only: [:show, :edit]
 
   def show
   @user = User.find(params[:id])

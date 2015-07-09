32731723960ab564836f15290cc2406b494c707c
David Pryce <dpryce@seek.com.au>
2015-07-10 09:00:05 +1000

    enable user to edit account

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,11 +14,26 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      session[:user_id] = @user.id
       flash[:success] = "Welcome to the Authentication and Authorisation App!"
+      session[:user_id] = @user.id
       redirect_to @user
     else
       render 'new'
+    end
+  end
+
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      flash[:success] = "Profile updated"
+      redirect_to @user
+    else
+      render 'edit'
     end
   end
 
